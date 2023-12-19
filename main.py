f396dc983a2245100c1a3620b5bbc978f324cb72
e1win <e1win@mail.ru>
2023-12-19 14:57:18 +0300

    added task 5 with carriages

--- main.py
+++ main.py
@@ -10,9 +10,20 @@
 # print("We neen", math.ceil(T), "days.") 
 
 #task3
-import math
-a = int(input("How many children in \"A\" class? "))
-b = int(input("How many children in \"B\" class? "))
-c = int(input("How many children in \"C\" class? "))
+# import math
+# a = int(input("How many children in \"A\" class? "))
+# b = int(input("How many children in \"B\" class? "))
+# c = int(input("How many children in \"C\" class? "))
 
-table = print("We need minimum", math.ceil((a+b+c)/2), "tables")
+# # table = print("We need minimum", math.ceil((a+b+c)/2), "tables")
+# table = print("We need minimum", ((a+1)//2 + (b+1)//2 + (c+1)//2), "tables")
+
+# task5
+
+a = int(input("In which carriage from the head Michael sat? "))
+b = int(input("Which number has his carriage? "))
+
+if a == b:
+    print(0)
+else:
+    print("The train has", a+b-1, "carriages")
