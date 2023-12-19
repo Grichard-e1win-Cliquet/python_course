d56972875d81ae1e417a0cb8c45678cc9671618e
e1win <e1win@mail.ru>
2023-12-19 14:26:18 +0300

    added task 1 and 3 with math.ceil

---
+++ main.py
@@ -0,0 +1,18 @@
+#task1
+
+# import math
+# n = int(input("Enter n: "))
+
+# m = int(input("Enter m: "))
+
+# T = float(m/n)  # (m + n - 1) // n
+
+# print("We neen", math.ceil(T), "days.") 
+
+#task3
+import math
+a = int(input("How many children in \"A\" class? "))
+b = int(input("How many children in \"B\" class? "))
+c = int(input("How many children in \"C\" class? "))
+
+table = print("We need minimum", math.ceil((a+b+c)/2), "tables")
