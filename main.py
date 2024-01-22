2aeba4936513a3682789a44780dc076134a87e44
e1win <e1win@mail.ru>
2024-01-22 22:57:21 +0300

    added task 32

--- main.py
+++ main.py
@@ -693,11 +693,20 @@
 # значения которых принадлежат заданному диапазону (т.е. не
 # меньше заданного минимума и не больше заданного
 # максимума)
-# Ввод: [-5, 9, 0, 3, -1, -2, 1,
-# 4, -2, 10, 2, 0, -9, 8, 10, -9,
-# 0, -5, -5, 7]
+# Ввод: [-5, 9, 0, 3, -1, -2, 1, 4, -2, 10, 2, 0, -9, 8, 10, -9, 0, -5, -5, 7]
 # Вывод: [1, 9, 13, 14, 19]
+# import random
+# A = list(random.randint(-99,100) for i in range(int(input('Enter a quantity of elements in list: '))))
+# print(A)
 
+# low, high = map(int, input('Enter a low and a high borders: ').split())
+
+# B = list()
+
+# for i in range(len(A)):
+#     if high > A[i] > low:
+#         B.append(i)
+# print(B)
 
 #task 33
 # Хакер Василий получил доступ к классному журналу и
